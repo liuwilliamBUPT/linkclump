0facd0715fc450f52da9ebad3afefb7cf7f29032
William <liuwilliam.zh@gmail.com>
2025-01-19 19:20:02 +0800

    refactor: remove old file

--- src-test/mock.js
+++
@@ -1,8 +0,0 @@
-/* eslint no-global-assign: 0, no-undef: 0 */
-
-chrome = {};
-chrome.extension = function () {};
-chrome.runtime.onMessage = function () {};
-chrome.runtime.onMessage.addListener = function () {};
-chrome.windows = function () {};
-chrome.windows.create = function () {};
